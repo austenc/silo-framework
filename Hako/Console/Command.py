d9851acc50acb1b4925be69cc31482f8b1cafc7b
austenc <austen.cameron@gmail.com>
2019-02-26 23:50:35 -0700

    added CLI colors via sty

--- Hako/Console/Command.py
+++ Hako/Console/Command.py
@@ -1,4 +1,5 @@
 import getpass
+from sty import fg, bg, rs
 
 class Command:
     """Handles arguments and options of CLI commands.
@@ -78,16 +79,19 @@ class Command:
         return self.options().get(key)
 
     def ask(self, question):
-        return input(self.end_with_newline(question))
+        return input(self.end_with_prompt(question))
     
     def secret(self, question):
-        return getpass.getpass(question)
+        return getpass.getpass(self.end_with_prompt(question))
     
     def confirm(self, question, default=False):
         yes = ['yes', 'y', 'ye']
         no = ['no', 'n']
         selected = 'yes' if default is True else 'no'
-        choice = input(question + ' (yes/no)' + ' [' + selected + '] ').lower()
+        choice = input('{question} (yes/no) [{selected}]\n> '.format(
+            question=question, 
+            selected=self.color(selected, 'warn')
+        )).lower()
         if choice is '':
             choice = selected
         if choice in yes:
@@ -108,17 +112,45 @@ class Command:
         elif int(choice) < len(choices):
             return choices[int(choice)]
 
-        print('Invalid choice, please try again.\n')
+        print(self.color('Invalid choice, please try again.', 'error'))
         return self.choice(question, choices, default)
 
     def list_choices(self, question, choices, selected):
-        output = '{q} [{selected}]: \n'.format(q=question, selected=selected)
+        output = '{question} '.format(question=self.color(question, 'info'))
+        output += '[{choice}]:\n'.format(choice=self.color(selected, 'warn'))
         for key, choice in enumerate(choices):
-            output += ' [{key}] {choice} \n'.format(key=key, choice=choice)
+            output += ' [{key}] {choice} \n'.format(key=self.color(key, 'warn'), choice=choice)
         output += '> '
         return output
 
-    def end_with_newline(self, string):
+    def end_with_prompt(self, string):
         if not string.endswith("\n"):
-            string += "\n"
+            string += "\n> "
         return string
+    
+    def color(self, text, color='info'):
+        if color is 'info':
+            return self.text_color(text, 'green')
+        if color is 'warn':
+            return self.text_color(text, 'yellow')
+        if color is 'question':
+            return self.text_color(text, 'black', 'cyan')
+        if color is 'error':
+            return self.text_color(text, 'white', 'da_red')
+
+    def text_color(self, string, _fg, _bg=None):
+        if _bg is None:
+            return '{fg}{s}{rs}'.format(fg=fg[_fg], s=string, rs=rs.all)
+        return '{fg}{bg}{s}{rs}'.format(fg=fg[_fg], bg=bg[_bg], s=string, rs=rs.all)
+
+    def info(self, text):
+        print(self.color(text, 'info'))
+
+    def warn(self, text):
+        print(self.color(text, 'warn'))
+    
+    def question(self, text):
+        print(self.color(text, 'question'))
+
+    def error(self, text):
+        print(self.color(text, 'error'))
